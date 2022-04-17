f97646ab99a41924ce49f44dbf6b9b17142cb320
IlyasAbylkassymov <103873882+IlyasAbylkassymov@users.noreply.github.com>
2022-04-17 21:33:29 +0600

    Add files via upload

--- BankAccount.cs
+++ BankAccount.cs
@@ -7,144 +7,49 @@ namespace Shekelstein_Bank_App
     {
         public string name = "";
         public string surname = "";
-        public bool isNameAndSurnameInCorrectFormat = true;
         public int balance = 0;
-        public string Name
+        public BankAccount(string surname, string name)
         {
-            get { return name; }
-            set {
-                isNameAndSurnameInCorrectFormat = true;
-                if (!string.IsNullOrEmpty(value) && value.All(char.IsLetter) && !value.Any(char.IsSeparator) && !value.Any(char.IsWhiteSpace))
-                {
-                    name = value;   
-                }
-                else
-                {
-                    Console.WriteLine("Некорректный формат имени.");
-                    isNameAndSurnameInCorrectFormat = false;
-                }
+            this.surname = surname;
+            this.name = name;
+        }
+        public void Deposit()
+        {
+            Console.WriteLine("Введите сумму для вноса на счет:");
+            string? stringBalance = Console.ReadLine();
+            if (int.TryParse(stringBalance, out int result) && Convert.ToInt32(stringBalance) > 0)
+            {
+                balance += Convert.ToInt32(stringBalance);
+                Console.WriteLine("Пополнение баланса, ожидайте...");
+                Thread.Sleep(5000);
+                Console.WriteLine($"Баланс успешно пополнен. На вашем счету {balance} тенге");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод суммы");
             }
         }
-        public string Surname
+        public void Withdraw()
         {
-            get { return surname; }
-            set
+            Console.WriteLine("Введите сумму для снятия со счета:");
+            string? stringBalance = Console.ReadLine();
+            if (int.TryParse(stringBalance, out int result) && Convert.ToInt32(stringBalance) > 0)
             {
-                isNameAndSurnameInCorrectFormat = true;
-                if (!string.IsNullOrEmpty(value) && value.All(char.IsLetter) && !value.Any(char.IsSeparator) && !value.Any(char.IsWhiteSpace))
+                if (balance >= Convert.ToInt32(stringBalance))
                 {
-                    surname = value;
+                    balance -= Convert.ToInt32(stringBalance);
+                    Console.WriteLine("Снятие средств, ожидайте...");
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"Средства успешно сняты. На вашем счету {balance} тенге.");
                 }
                 else
                 {
-                    Console.WriteLine("Некорректный формат фамилии.");
-                    isNameAndSurnameInCorrectFormat = false;
-                }
-            }
-        }
-
-        public BankAccount()
-        {
-
-        }
-        public void MenuShowUp()
-        {
-            Console.WriteLine("Меню.");
-            Console.WriteLine("1. Информация о счёте.");
-            Console.WriteLine("2. Внести средства на счёт.");
-            Console.WriteLine("3. Снять средства со счёта");
-            Console.WriteLine("4. Выйти");
-            string? choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        Console.WriteLine($"ФИО: {surname} {name}");
-                        Console.WriteLine($"На счету на данный момент {balance} тенге.");
-                        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
-                        Console.ReadKey();
-                        MenuShowUp();
-                        break;
-                    case "2":
-                        Console.WriteLine("Введите сумму для вноса на счет:");
-                        string? stringBalance = Console.ReadLine();
-                        if(int.TryParse(stringBalance, out int result) && Convert.ToInt32(stringBalance) > 0)
-                        {
-                                balance += Convert.ToInt32(stringBalance);
-                                Console.WriteLine("Пополнение баланса, ожидайте...");
-                                Thread.Sleep(5000);
-                                Console.WriteLine($"Баланс успешно пополнен. На вашем счету {balance} тенге");
-                                MenuShowUp();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Некорректный ввод суммы");
-                            MenuShowUp();
-                        }
-                        
-                        break;
-                    case "3":
-                        Console.WriteLine("Введите сумму для снятия со счета:");
-                        stringBalance = Console.ReadLine();
-                        if (int.TryParse(stringBalance, out result) && Convert.ToInt32(stringBalance)>0)
-                        {
-                                if (balance >= Convert.ToInt32(stringBalance))
-                                {
-                                    balance -= Convert.ToInt32(stringBalance);
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"Недостаточно средств. На вашем счету {balance} тенге.");
-                                    MenuShowUp();
-                                    break;
-                                }
-                                Console.WriteLine("Снятие средств, ожидайте...");
-                                Thread.Sleep(5000);
-                                Console.WriteLine($"Средства успешно сняты. На вашем счету {balance} тенге.");
-                                MenuShowUp();
-                            
-                        }
-                        else
-                        {
-                            Console.WriteLine("Некорректный ввод суммы");
-                            MenuShowUp();
-                        }
-                        break;
-                    case "4":
-                        Console.WriteLine("Хорошего дня.");
-                        Console.WriteLine("Выход...");
-                        break;
-                    default:
-                        Console.WriteLine("Некорректный выбор пункта.");
-                        MenuShowUp();
-                        break;
-                }
-        }
-        public void StartingUp()
-        {
-
-            Console.WriteLine("Введите ваши данные.");
-            Console.WriteLine("Фамилия: ");
-            Surname = Console.ReadLine();
-            if(isNameAndSurnameInCorrectFormat)
-            {
-                Console.WriteLine("Имя: ");
-                Name = Console.ReadLine();
-                if(isNameAndSurnameInCorrectFormat)
-                {
-                    Console.WriteLine($"Здравствуйте, {surname} {name}");
-                    MenuShowUp();
-                }
-                else
-                {
-                    Console.WriteLine("Попробуйте ещё раз");
-                    StartingUp();
+                    Console.WriteLine($"Недостаточно средств. На вашем счету {balance} тенге.");
                 }
             }
             else
             {
-                Console.WriteLine("Попробуйте ещё раз");
-                StartingUp();
+                Console.WriteLine("Некорректный ввод суммы");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -7,10 +7,67 @@ namespace Shekelstein_Bank_App
         
         public static void Main(string[] args)
         {
-            Console.WriteLine("Добро пожаловать в приложения банка Шекельштейн.");
-            BankAccount bankAccount = new BankAccount();
-            bankAccount.StartingUp();
+            
+            Console.WriteLine("Добро пожаловать в приложение банка Шекельштейн.");
+            MenuLogic("", "");
         }
+        private static void MenuLogic(string surname, string name)
+        {
+            string? choice = null;
+            Console.WriteLine("Введите ваши данные.");
+            Console.WriteLine("Фамилия: ");
+            surname = Console.ReadLine().Trim();
+            if (!string.IsNullOrEmpty(surname) && surname.All(char.IsLetter))
+            {
+                Console.WriteLine("Имя:");
+                name = Console.ReadLine().Trim();
+                if (!string.IsNullOrEmpty(name) && name.All(char.IsLetter))
+                {
+                    BankAccount bankAccount = new BankAccount(surname, name);
+                    
+                    do {
+                        Console.WriteLine("Меню.");
+                        Console.WriteLine("1. Информация о счёте.");
+                        Console.WriteLine("2. Внести средства на счёт.");
+                        Console.WriteLine("3. Снять средства со счёта");
+                        Console.WriteLine("4. Выйти");
+                        choice = Console.ReadLine();
+                        switch (choice)
+                        {
+                            case "1":
+                                Console.WriteLine($"ФИО: {bankAccount.surname} {bankAccount.name}");
+                                Console.WriteLine($"На счету на данный момент {bankAccount.balance} тенге.");
+                                break;
+                            case "2":
+                                bankAccount.Deposit();
+                                break;
+                            case "3":
+                                bankAccount.Withdraw();
+                                break;
+                            case "4":
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный выбор пункта.");
+                                break;
+                        }
+                    }
+                    while (choice != "4");
+                    Console.WriteLine("Хорошего дня.");
+                    Console.WriteLine("Выход...");
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный формат имени.");
+                    MenuLogic("", "");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный формат фамилии.");
+                MenuLogic("", "");
+            }
+        }
+
     }
     
 }
